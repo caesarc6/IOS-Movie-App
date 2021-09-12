7bc07ee2808ca7786edc4c77d369313eac8e298c
caesarc6 <46427941+caesarc6@users.noreply.github.com>
2021-09-11 23:56:58 -0400

    Updated w. Required User Stories

---
+++ Movie/MovieCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieCell.swift
+//  Movie
+//
+//  Created by Caesar Cuzco on 9/11/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synosisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+}

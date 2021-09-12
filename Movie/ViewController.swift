9fd3a33132ba121516a6c4a9e76fc8616b1fcb12
caesarc6 <46427941+caesarc6@users.noreply.github.com>
2021-09-11 20:16:13 -0400

    Initial Commit

---
+++ Movie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie
+//
+//  Created by Caesar Cuzco on 9/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
